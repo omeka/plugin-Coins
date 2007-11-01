2a3a7565fd3e4c92d755575dfdd070a2998d843d
Jim Safley <jimsafley@gmail.com>
2007-11-01 21:46:05 +0000

    changed code from procedural to OOP. added mapping notes.

--- plugin.php
+++ plugin.php
@@ -1,79 +1,197 @@
 <?php
 add_plugin_hook('append_to_item_show', 'COinS');
 
-
 function COinS($item)
 {
-    // @todo: devise a better mapping scheme between omeka and COinS/Zotero
-    switch ($item->Type->name) {
-        case 'Oral History':
-             $rft_type = 'interview';
-             break;
-        case 'Moving Image':
-            $rft_type = 'videoRecording';
-            break;
-        case 'Sound':
-            $rft_type = 'audioRecording';
-            break;
-        case 'Website':
-            $rft_type = 'webPage';
-            break;
-        case 'Email':
-            $rft_type = 'e-mail';
-            break;
-        case 'Hyperlink':
-            $rft_type = 'webPage';
-            break;
-        case 'Document':
-        case 'Event':
-        case 'Lesson Plan':
-        case 'Person':
-        case 'Interactive Resource':
-        case 'Still Image':
-        default:
-            $rft_type = 'document';
-            break;
-    }
-    
-    $rft_title          = $item->title;
-    $rft_creator        = $item->creator;
-    $rft_subject        = $item->subject;
-    $rft_description    = $item->description;
-    $rft_publisher      = $item->publisher;
-    $rft_contributor    = $item->contributor;
-    $rft_date           = $item->date;
-    // @todo: include format
-    $rft_format         = '';//$item->format;
-    $rft_identifier     = WEB_ROOT . DIRECTORY_SEPARATOR . 'item' . DIRECTORY_SEPARATOR . 'show' . DIRECTORY_SEPARATOR . $item->id;
-    $rft_source         = $item->source;
-    // @todo: somehow include spacial coverage?
-    $rft_coverage       = $item->temporal_coverage_start . '–' . $item->temporal_coverage_end;
-    $rft_rights         = $item->rights;
-    
-    $COinS = array(
-        'ctx_ver'           => 'Z39.88-2004', 
-        'rft_val_fmt'       => 'info:ofi/fmt:kev:mtx:dc', 
-        'rfr_id'            => 'info:sid/omeka.org:generator', 
-        'rft.title'         => $rft_title, 
-        'rft.creator'       => $rft_creator, 
-        'rft.subject'       => $rft_subject, 
-        'rft.description'   => $rft_description, 
-        'rft.publisher'     => $rft_publisher, 
-        'rft.contributor'   => $rft_contributor, 
-        'rft.date'          => $rft_date, // yyyy-mm-dd
-        'rft.type'          => $rft_type, 
-        'rft.format'        => $rft_format, 
-        'rft.identifier'    => $rft_identifier, 
-        'rft.source'        => $rft_source, 
-        'rft.language'      => $rft_language, 
-        'rft.coverage'      => $rft_coverage, 
-        'rft.rights'        => $rft_rights
-    );
-?>
-<span 
-    class="Z3988" 
-    title="<?php echo http_build_query($COinS, '', '&amp;'); ?>">
-</span>
-<?php
+    $coins = new COinS($item);
+    echo $coins->coinsSpan;
 }
+
+class COinS
+{
+    private $item;
+    
+    private $coins = array();
+    
+    const ctx_ver     = 'Z39.88-2004';
+    const rft_val_fmt = 'info:ofi/fmt:kev:mtx:dc';
+    const rfr_id      = 'info:sid/omeka.org:generator';
+    
+    public $coinsSpan;
+    
+    public function __construct($item)
+    {
+        $this->item = $item;
+        
+        $this->coins['ctx_ver']     = self::ctx_ver;
+        $this->coins['rft_val_fmt'] = self::rft_val_fmt;
+        $this->coins['ctx_ver']     = self::ctx_ver;
+        $this->coins['rfr_id']      = self::rfr_id;
+        
+        $this->setTitle();
+        $this->setCreator();
+        $this->setSubject();
+        $this->setDescription();
+        $this->setPublisher();
+        $this->setContributor();
+        $this->setDate();
+        $this->setType();
+        $this->setFormat();
+        $this->setIdentifier();
+        $this->setSource();
+        $this->setLanguage();
+        $this->setCoverage();
+        $this->setRights();
+        
+        $this->buildCoinsSpan();
+    }
+    private function setTitle()
+    {
+        $this->coins['rft.title'] = $this->item->title;
+    }
+    private function setCreator()
+    {
+        $this->coins['rft.creator'] = $this->item->creator;
+    }
+    private function setSubject()
+    {
+        $this->coins['rft.subject'] = $this->item->subject;
+    }
+    private function setDescription()
+    {
+        // Truncate to avoid long descriptions.
+        $this->coins['rft.description'] = substr($this->item->description, 0, 500);
+    }
+    private function setPublisher()
+    {
+        $this->coins['rft.publisher'] = $this->item->publisher;
+    }
+    private function setContributor()
+    {
+        $this->coins['rft.contributor'] = $this->item->contributor;
+    }
+    private function setDate()
+    {
+        $this->coins['rft.date'] = $this->item->date;
+    }
+    private function setType()
+    {
+        // @todo: devise a better mapping scheme between omeka and COinS/Zotero
+        switch ($this->item->Type->name) {
+            case 'Oral History':
+                $type = 'interview';
+                break;
+            case 'Moving Image':
+                $type = 'videoRecording';
+                break;
+            case 'Sound':
+                $type = 'audioRecording';
+                break;
+            case 'Email':
+                $type = 'email';
+                break;
+            case 'Website':
+            case 'Hyperlink':
+                $type = 'webPage';
+                break;
+            case 'Document':
+            case 'Event':
+            case 'Lesson Plan':
+            case 'Person':
+            case 'Interactive Resource':
+            case 'Still Image':
+            default:
+                $type = 'document';
+                break;
+        }
+        $this->coins['rft.type'] = $type;
+    }
+    private function setFormat()
+    {
+        // @todo: include format when Omeka is updated to include it.
+        $this->coins['rft.format'] = ''; //$this->item->format;
+    }
+    private function setIdentifier()
+    {
+        $this->coins['rft.identifier'] = $_SERVER['SCRIPT_URI'];
+    }
+    private function setSource()
+    {
+        $this->coins['rft.source'] = $this->item->source;
+    }
+    private function setLanguage()
+    {
+        $this->coins['rft.language'] = $this->item->language;
+    }
+    private function setCoverage()
+    {
+        // @todo: somehow include spacial coverage?
+        $this->coins['rft.coverage'] = $this->item->temporal_coverage_start . '–' . $this->item->temporal_coverage_end;
+    }
+    private function setRights()
+    {
+        $this->coins['rft.rights'] = $this->item->rights;
+    }
+    private function buildCoinsSpan()
+    {
+        $this->coinsSpan = '<span class="Z3988" title="' . http_build_query($this->coins, '', '&amp;') . '"></span>';
+    }
+}
+
+/*
+// Zotero item types as of 2007-11-01
+note
+book
+bookSection
+journalArticle
+magazineArticle
+newspaperArticle
+thesis
+letter
+manuscript
+interview
+film
+artwork
+webpage
+attachment
+report
+bill
+case
+hearing
+patent
+statute
+email
+map
+blogPost
+instantMessage
+forumPost
+audioRecording
+presentation
+videoRecording
+tvBroadcast
+radioBroadcast
+podcast
+computerProgram
+conferencePaper
+document
+encyclopediaArticle
+dictionaryEntry
+*/
+
+/*
+// Omeka item types (bundled) as of 2007-11-01
+Oral History
+Moving Image
+Sound
+Email
+Website
+Hyperlink
+Document
+Event
+Lesson Plan
+Person
+Interactive Resource
+Still Image
+*/
+
 ?>
