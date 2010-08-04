53664e600f572921bf09b289347634ee57b4689e
John Flatness <john@zerocrates.org>
2010-08-04 13:39:54 +0000

    Adds AllTests case class.

---
+++ tests/AllTests.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2007-2010
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ */
+
+require_once 'Coins_Test_AppTestCase.php';
+
+/**
+ * Test suite for Coins.
+ *
+ * @package Omeka
+ * @copyright Center for History and New Media, 2007-2010
+ */
+class Coins_AllTests extends PHPUnit_Framework_TestSuite
+{
+    public static function suite()
+    {
+        $suite = new Coins_AllTests('Coins Tests');
+        $testCollector = new PHPUnit_Runner_IncludePathTestCollector(
+          array(dirname(__FILE__) . '/integration')
+        );
+        $suite->addTestFiles($testCollector->collectTests());
+        return $suite;
+    }
+}
