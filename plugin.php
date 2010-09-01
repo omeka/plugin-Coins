c9f74f23241f700efb89d3f9e2b435c5dd0aacc9
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-09-01 18:24:33 +0000

    Fixes fatal error in Coins test by requiring the theme helpers in order to accurately represent the plugin's dependencies.

--- plugin.php
+++ plugin.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2007-2010
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Coins
+ */
+
+
+// Coins class uses some theme helper functions, so need to include this.
+require_once HELPERS;
 
 add_plugin_hook('install', 'coins_install');
 add_plugin_hook('uninstall', 'coins_uninstall');
